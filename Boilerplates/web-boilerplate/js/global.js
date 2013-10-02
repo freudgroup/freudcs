3c75ebb7486069e73af35c672e5653f12876b829
Alberto Gil <gilalberto@gmail.com>
2013-10-01 20:54:29 -0600

    Fixing tabs for 4 spaces, on the CSS adding media queries, html changes for HTML5 on the <main> and on the JS adding JS Docs and making it more JSLINT valid

--- Boilerplates/web-boilerplate/js/global.js
+++ Boilerplates/web-boilerplate/js/global.js
@@ -12,38 +12,48 @@ var clientName = window.clientName || {};
  * @namespace
  */
 (function (context, $) {
-	
-	var vars = {
-			a : 0,
-			b : 1
-		};
-	
-	var utils = {
-		
-		/**
-		 * Description
-		 */
-		method1 : function() {
-			
-		}
-	};
-	
-	/**
-	 * Description
-	 * 
-	 * @return a lovely message.
-	 */
-	context.publicMethod = function () {
-		return "I'm scared";
-	}
-	
-	/**
-	 * Initializes the module.
-	 */
-	function init() {
-		
-	}
-	
-	$(init);
 
-})(clientName, jQuery);
+    'use strict';
+
+    var vars = {
+            a : 0,
+            b : 1
+        };
+
+    var utils = {
+
+        /**
+         * Description of my private method for utils.
+         *
+         * @param param1 {Object} Sample 1
+         * @param param2 {Object} Sample 2 of another object
+         *
+         * @private
+         */
+        method1 : function (param1, param2) {
+            param2 = param1;
+            param1 = param2;
+        }
+    };
+
+    /**
+     * Description of a public method to be used outside of the module.
+     * 
+     * @return {String} a lovely message.
+     * @public
+     */
+    context.publicMethod = function () {
+        return "I'm scared";
+    };
+
+    /**
+     * Initializes the module.
+     * @private
+     */
+    function init() {
+        alert("Initializing module...");
+    }
+
+    $(init);
+
+}(clientName, jQuery));
